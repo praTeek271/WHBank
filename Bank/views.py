7b2f652f3f230450b1a29a77395ee16af9fb7f7f
starkvr1 <prateekpatriotasme@gmail.com>
2022-02-11 04:36:20 +0530

    2nd last commit

--- Bank/views.py
+++ Bank/views.py
@@ -37,28 +37,28 @@ def Member(request,acc): #register function
 
  #------------------------ Transaction-Form------------------------------------
     print("------------>",request.user.username)   
+
     if (request.user.username!=(None or "")):
         if request.method=="POST":
             rname=request.POST.get("rname")
-            if (rname in CustomerAccount.objects.values('name')):
-                remail=request.POST.get("rEmail")
-                if (remail in member.email):
+            if (rname in db_check('name')):
+                remail=request.POST.get("remail")
+                if (remail in db_check('email')):
                     sname=request.user.username
                     semail=request.user.username+'@gmail.com'
                     sphone=request.POST.get('sphoneno')
-                    rphone=request.POST.get("rphoneno")
-                    if(rphone in member.phoneno and sphone in member.phoneno):
+                    if(sphone in db_check('phoneno')):
                         sammount=request.POST.get('sammount')
-                        if (member.totalbalance >sammount):
-                            transfer_detail=transectiondetail(recievername=rname,recieveremail=remail,sendername=sname,senderemail=semail,deducted_amt=sammount,account_balance=(member.totalbalance-sammount))
+                        if (member.totalbalance > int(sammount)):
+                            transfer_detail=transectiondetail(recievername=rname,recieveremail=remail,sendername=sname,senderemail=semail,money_amt=int(sammount))
                             transfer_detail.save()
                             member1=CustomerAccount.objects.get(name=rname) # Reciever`s id
                             print(member1)
-                            member1.totalbalance=(member1.totalbalance+sammount)
+                            member1.totalbalance=(member1.totalbalance+int(sammount))
 
                             member2=CustomerAccount.objects.get(name=sname) # Sender`s id
                             print(member2)
-                            member2.totalbalance=(member1.totalbalance-sammount)
+                            member2.totalbalance=(member1.totalbalance-int(sammount))
                             try:
                                 member1.save() #    <----------------,
                                 member2.save() #    <----------------'............(save them in the database)
@@ -85,6 +85,12 @@ def Member(request,acc): #register function
 #   |   |         and save in their respective objects so that they can be fetched for showing            |
 #   +-----------------------------------------**_**-------------------------------------------------------+
 
+def db_check(dbdtls):
+    data=dbdtls
+    s=CustomerAccount.objects.values(data)
+    l1=[i for i in s]
+    l2=[l1[j].get(data) for j in range(len(l1))]
+    return(l2)
 
 def transaction_log(request,user):
     try:
